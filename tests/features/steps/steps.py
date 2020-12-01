693cdbda0c63952fa2704eaa19e0e928dbe373df
Tristan Chambers <tchambers@smith.edu>
2020-12-01 16:32:20 -0500

    Fix failing test on second iteration!

--- tests/features/steps/steps.py
+++ tests/features/steps/steps.py
@@ -2,6 +2,7 @@ from behave import given, when, then
 from lxml import etree, objectify
 import subprocess
 import os
+import sys
 import time
 import json
 from asnake.aspace import ASpace
@@ -10,6 +11,8 @@ import tempfile
 
 logging.basicConfig(level=logging.INFO)
 
+EXPORTER_PATH = os.path.join(os.path.join(os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir), os.pardir), os.pardir)
+EXPORTER_PY_FILE = 'export.py'
 
 @given('I set the title of the Digital Object to "{title}"')
 def step_impl(context, title):
@@ -19,7 +22,7 @@ def step_impl(context, title):
 	rec = aspace.client.get(uri[0]).json()
 	rec['title'] = title
 	aspace.client.post(rec['uri'], json=rec)
-	
+
 
 @given('I set the title of the Archival Object to "{title}"')
 def step_impl(context, title):
@@ -54,13 +57,20 @@ def step_impl(context, do_id):
 @when('I run the exporter')
 def step_imp(context):
 	context.temp_export_output_dir = tempfile.TemporaryDirectory()
-	os.chdir('../')
-	os.system('python3 export.py %s' % context.temp_export_output_dir.name)
-	os.system('ls %s' % context.temp_export_output_dir.name)
-	# import pdb, sys; pdb.Pdb(stdout=sys.__stdout__).set_trace()
-	with open(context.temp_export_output_dir.name + '/' + context.filename + '.xml', 'rb') as fobj:
-		xml = fobj.read()
-
+	os.chdir(EXPORTER_PATH)
+	shell_command = [sys.executable, EXPORTER_PY_FILE, context.temp_export_output_dir.name]
+	result = subprocess.run(shell_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	if result.returncode != 0:
+		logging.error("export.py error: %s" % result.stderr.decode())
+		logging.error("export.py completed %s" % result.stdout.decode())
+	else:
+		logging.debug("export.py completed %s" % result.stdout.decode())
+	# import pdb; pdb.Pdb(stdout=sys.__stdout__).set_trace()
+	try:
+		with open(context.temp_export_output_dir.name + '/' + context.filename + '.xml', 'rb') as fobj:
+			xml = fobj.read()
+	except FileNotFoundError as e:
+		assert False, "Could not find XML output file %s" % e
 	context.xml_output_tree = etree.XML(xml)
 
 
@@ -78,5 +88,5 @@ def step_impl(context, title):
 
 @then('I should see an <identifier> tag with an attribute of local that reads "{do_id}"')
 def step_impl(context, do_id):
-	tag = context.xml_output_tree.xpath('x:identifier[@type=local]', namespaces={'x':'http://www.loc.gov/mods/v3'})[0].text
+	tag = context.xml_output_tree.xpath('x:identifier[@type="local"]', namespaces={'x':'http://www.loc.gov/mods/v3'})[0].text
 	assert tag == do_id, 'Tag text not as expected! Wanted: {}. Returned: {}'.format(do_id, tag)
