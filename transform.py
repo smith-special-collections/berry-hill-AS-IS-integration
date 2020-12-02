ecdef0a5155e8fdd11d927d0e2a21e01c8ba68fd
cmarshall <cmarshall@smith.edu>
2020-12-02 11:34:50 -0600

    Comments added, deduped Agents, added deepcopies, fixed arrangement and date errors

--- transform.py
+++ transform.py
@@ -1,3 +1,8 @@
+from copy import deepcopy
+import re
+import html
+from pprint import pprint as pp
+
 MAPPING = {
     'title': {                       # The name of the field as will appear in the template context
         'transform_function': 'title', # The name of the function that will generate the field contents
@@ -32,7 +37,7 @@ MAPPING = {
         'required': True
     },
     'subjects': {
-        'transform_function': 'subjects',
+        'transform_function': 'subjects_with_genre_forms_removed',
         'required': False
     },
     'genre_subjects': {
@@ -106,6 +111,10 @@ MAPPING = {
     'top_container': {
         'transform_function': 'top_container',
         'required': False
+    },
+    'excerpts': {
+        'transform_function': 'excerpts',
+        'required': False
     }
 }
 
@@ -118,8 +127,10 @@ class Transforms():
     saved to the template_context data structure.
     """
 
+
     def title(self, EXTRACTED_DATA, do_id):
-        return EXTRACTED_DATA['digital_objects'][do_id]['title']
+        title = EXTRACTED_DATA['digital_objects'][do_id]['title']
+        return title
 
 
     def digital_object_id(self, EXTRACTED_DATA, do_id):
@@ -139,14 +150,17 @@ class Transforms():
                 return ao_uri
 
     
-    def archival_object(self, EXTRACTED_DATA, do_id):
+    def _archival_object(self, EXTRACTED_DATA, do_id):
+        '''Helper function that returns archival object record from EXTRACTED_DATA'''
         ao_uri = self.archival_object_uri(EXTRACTED_DATA, do_id)
         if ao_uri != None:
-            return EXTRACTED_DATA['archival_objects'][ao_uri]
+            # The digital object might not be attached to an archival object
+            # Wherever ao_uri != None, this might be the case
+            return deepcopy(EXTRACTED_DATA['archival_objects'][ao_uri])
 
 
     def archival_object_ref(self, EXTRACTED_DATA, do_id):
-        ao = self.archival_object(EXTRACTED_DATA, do_id)
+        ao = self._archival_object(EXTRACTED_DATA, do_id)
         if ao != None:
             ao_ref = ao['ref_id']
             return ao_ref
@@ -160,26 +174,49 @@ class Transforms():
             return resource_uri
   
     
-    def resource(self, EXTRACTED_DATA, do_id):
+    def _resource(self, EXTRACTED_DATA, do_id):
+        '''Helper function that returns resource record from EXTRACTED_DATA'''
         resource_uri = self.resource_uri(EXTRACTED_DATA, do_id)
         if resource_uri != None:
-            return EXTRACTED_DATA['resources'][resource_uri]
+            return deepcopy(EXTRACTED_DATA['resources'][resource_uri])
 
     
     def resource_title(self, EXTRACTED_DATA, do_id):
-        resource = self.resource(EXTRACTED_DATA, do_id)
+        resource = self._resource(EXTRACTED_DATA, do_id)
         if resource != None:
+            # There might not be a resource to which the digital object is related
+            # Wherever 'resource != None', this is the case
             return resource['title']
 
     
     def resource_ms_no(self, EXTRACTED_DATA, do_id):
-        resource = self.resource(EXTRACTED_DATA, do_id)
+        resource = self._resource(EXTRACTED_DATA, do_id)
         if resource != None:
             ms_no = resource['id_0'] + ' ' + resource['id_1'] + ' ' + resource['id_2']
             return ms_no
 
 
-    def subjects(self, EXTRACTED_DATA, do_id):
+    def subjects_with_genre_forms_removed(self, EXTRACTED_DATA, do_id):
+        '''F to remove genre subjects from subject data so they do not get templated incorrectly'''
+        subjects = self._subjects(EXTRACTED_DATA, do_id)
+        cleaned_subjects = []
+        if len(subjects) > 0:
+            for sub in subjects:
+                for term in sub['terms']:
+                    if term['term_type'] == 'genre_form':
+                        continue
+                    else:
+                        if sub not in cleaned_subjects:
+                            cleaned_subjects.append(sub)
+        if len(cleaned_subjects) == 0:
+            return None
+        else:
+            return cleaned_subjects
+
+
+    def _subjects(self, EXTRACTED_DATA, do_id):
+        '''A helper function to get all the subjects at the archival object level; logic based on rules of inheritance: 
+        https://github.com/smith-special-collections/sc-documentation/wiki/Rules-for-description-inheritance-for-digital-object-records'''
         subjects = []
         ao_uri = self.archival_object_uri(EXTRACTED_DATA, do_id)
         if ao_uri != None:
@@ -194,7 +231,7 @@ class Transforms():
 
 
     def genre_subjects(self, EXTRACTED_DATA, do_id):
-        subjects = self.subjects(EXTRACTED_DATA, do_id)
+        subjects = self._subjects(EXTRACTED_DATA, do_id)
         genre_subjects = []
         if len(subjects) > 0:
             for sub in subjects:
@@ -203,10 +240,14 @@ class Transforms():
                         if sub not in genre_subjects:
                             genre_subjects.append(sub)
 
-        return genre_subjects
+        if len(genre_subjects) == 0:
+            return None
+        else:
+            return genre_subjects
 
 
-    def set_mods_agent_type(self, agent_data):
+    def remap_mods_agent_type(self, agent_data):
+        '''Remaps jsonmodel_type to mods agent type in deepcopy'''
         if agent_data['jsonmodel_type'] == 'agent_person':
             agent_data['jsonmodel_type'] = 'personal'
         elif agent_data['jsonmodel_type'] == 'agent_corporate_entity':
@@ -218,9 +259,11 @@ class Transforms():
 
 
     def agents(self, EXTRACTED_DATA, do_id):
+        '''Logic based on rules of inheritance: 
+        https://github.com/smith-special-collections/sc-documentation/wiki/Rules-for-description-inheritance-for-digital-object-records'''
         agents = []
-        ao = self.archival_object(EXTRACTED_DATA, do_id)
-        resource = self.resource(EXTRACTED_DATA, do_id)
+        ao = self._archival_object(EXTRACTED_DATA, do_id)
+        resource = self._resource(EXTRACTED_DATA, do_id)
         if ao != None:
             # Get agents from archival object
             if len(ao['linked_agents']) > 0:
@@ -228,7 +271,7 @@ class Transforms():
                     agent_dict = {}
                     agent_dict['role'] = agent['role']
                     agent_uri = agent['ref']
-                    agent_dict['agent_data'] = self.set_mods_agent_type(EXTRACTED_DATA['agents'][agent_uri])
+                    agent_dict['agent_data'] = self.remap_mods_agent_type(deepcopy(EXTRACTED_DATA['agents'][agent_uri]))
                     agents.append(agent_dict)
         if resource != None:
             # Get agents from resource
@@ -238,9 +281,12 @@ class Transforms():
                         agent_dict = {}
                         agent_dict['role'] = agent['role']
                         agent_uri = agent['ref']
-                        agent_dict['agent_data'] = self.set_mods_agent_type(EXTRACTED_DATA['agents'][agent_uri])
+                        agent_dict['agent_data'] = self.remap_mods_agent_type(deepcopy(EXTRACTED_DATA['agents'][agent_uri]))
                         agents.append(agent_dict)
         
+        agents = [i for n, i in enumerate(agents) if i not in agents[n + 1:]]
+        pp(agents)
+        pp(len(agents))
         return agents
 
 
@@ -251,8 +297,11 @@ class Transforms():
             for agent in agents:
                 if agent['role'] == 'subject':
                     subject_agents.append(agent)
-
-        return subject_agents
+        # print(subject_agents)
+        if len(subject_agents) == 0:
+            return None
+        else:
+            return subject_agents
 
 
     def donor_agents(self, EXTRACTED_DATA, do_id):
@@ -262,8 +311,10 @@ class Transforms():
             for agent in agents:
                 if agent['role'] == 'source':
                     donor_agents.append(agent)
-
-        return donor_agents
+        if len(donor_agents) == 0:
+            return None
+        else:
+            return donor_agents
 
 
     def creator_agents(self, EXTRACTED_DATA, do_id):
@@ -273,38 +324,58 @@ class Transforms():
             for agent in agents:
                 if agent['role'] == 'creator':
                     creator_agents.append(agent)
-
-        return creator_agents
+        if len(creator_agents) == 0:
+            return None
+        else:
+            return creator_agents
 
 
     def extent(self, EXTRACTED_DATA, do_id):
-        ao = self.archival_object(EXTRACTED_DATA, do_id)
+        ao = self._archival_object(EXTRACTED_DATA, do_id)
         if ao != None:
             if len(ao['extents']) > 0:
                 return ao['extents'][0]
 
 
+    def remove_EAD_tags(self, note):
+        '''Helper function to remove HTML entities from note content'''
+        regex = '(<.*?>)'
+        try:
+            chars_to_remove = re.findall(regex, note)
+            if len(chars_to_remove) > 0:
+                for char in chars_to_remove:
+                    note = html.unescape(note)
+                    note = note.replace(char, '')
+                    note = html.escape(note)
+            return note      
+        except Exception as e:  
+            print(e)
+            return note
+
+
     def notes(self, EXTRACTED_DATA, do_id, note_type):
-        n = []
-        ao = self.archival_object(EXTRACTED_DATA, do_id)
+        '''Logic based on rules of inheritance: 
+        https://github.com/smith-special-collections/sc-documentation/wiki/Rules-for-description-inheritance-for-digital-object-records'''
+        notes_lst = []
+        ao = self._archival_object(EXTRACTED_DATA, do_id)
         if ao != None:
             if len(ao['notes']) > 0:
                 for note in ao['notes']:
                      if note['type'] == note_type:
                         if note['publish'] == True:
-                            n.append(note['subnotes'][0]['content'])
-        if len(n) == 0:
-            resource = self.resource(EXTRACTED_DATA, do_id)
+                            notes_lst.append(self.remove_EAD_tags(note['subnotes'][0]['content']))
+        if len(notes_lst) == 0:
+            # If there are not any notes at the archival object level, search at the resource level
+            resource = self._resource(EXTRACTED_DATA, do_id)
             if resource != None:
                 if len(resource['notes']) > 0:
                     for note in resource['notes']:
                         if note['publish'] == True:
                             if note['type'] == note_type:
-                                n.append(note['subnotes'][0]['content'])
+                                notes_lst.append(self.remove_EAD_tags(note['subnotes'][0]['content']))
+        return notes_lst
 
-        return n
 
-     
     def abstract(self, EXTRACTED_DATA, do_id):
         abstract = self.notes(EXTRACTED_DATA, do_id, 'scopecontent')
         return abstract
@@ -321,33 +392,38 @@ class Transforms():
 
 
     def arrangement(self, EXTRACTED_DATA, do_id):
-        arrangement = None
-        ao = self.archival_object(EXTRACTED_DATA, do_id)
+        '''Returns arrangement note'''
+        arrangement = []
+        ao = self._archival_object(EXTRACTED_DATA, do_id)
         if ao != None:
             if len(ao['notes']) > 0:
                 for note in ao['notes']:
                     if note['type'] == 'arrangement':
-                        arrangement = note['subnotes']
+                        arrangement.append(note['subnotes']) # Fix this to ensure more than one arrangement note
 
-        if arrangement != None:
+        # Creates a dictionary of components from the arrangement note if there is one that is then passed to self.arrangement_items
+        if len(arrangement) > 0:
             arrangement_dict = {}
-            arrangement_dict['content'] = arrangement[0]['content']
+            arrangement_dict['content'] = []
+            arrangement_dict['items'] = []
+            for a in arrangement:
+                arrangement_dict['content'].append(a['content'])
             try:
-                arrangement_dict['items'] = []
-                for item in arrangement[1]['items']:
+                for item in a['items']:
                     arrangement_dict['items'].append(item)
             except KeyError:
-                arrangement_dict['items'] = None
+                pass
             
             return arrangement_dict
-
-        return arrangement
+        
+        else:
+            return None
 
 
     def arrangement_items(self, EXTRACTED_DATA, do_id):
         arrangement = self.arrangement(EXTRACTED_DATA, do_id)
         if isinstance(arrangement, dict):
-            if arrangement['items'] != None:
+            if len(arrangement['items']) > 0:
                 return arrangement['items']
             else:
                 return None
@@ -355,50 +431,67 @@ class Transforms():
 
     def general_notes(self, EXTRACTED_DATA, do_id):
         general_notes = []
-        ao = self.archival_object(EXTRACTED_DATA, do_id)
+        ao = self._archival_object(EXTRACTED_DATA, do_id)
         if ao != None:
             if len(ao['notes']) > 0:
                 for note in ao['notes']:
                     if note['type'] == 'odd':
+                        # Odd is the term_type for general notes
                         if note['publish'] == True:
                             general_notes.append(note['subnotes'][0])
-
-        return general_notes
+       
+        if len(general_notes) == 0:
+            return None
+        else:
+            return general_notes
 
 
     def language(self, EXTRACTED_DATA, do_id):
+        '''Logic based on rules of inheritance: 
+        https://github.com/smith-special-collections/sc-documentation/wiki/Rules-for-description-inheritance-for-digital-object-records'''
         langs = []
-        ao = self.archival_object(EXTRACTED_DATA, do_id)
+        ao = self._archival_object(EXTRACTED_DATA, do_id)
         if ao != None:
             if len(ao['lang_materials']) > 0:
                 for lang in ao['lang_materials']:
                     try:
                         langs.append(lang['language_and_script']['language'])
                     except KeyError:
-                        continue
+                        pass
+                    except Exception as e:
+                        print(e)
+            if len(langs) == 0:
+                resource = self._resource(EXTRACTED_DATA, do_id)
+                if resource != None:
+                    if len(resource['lang_materials']) > 0:
+                        for lang in resource['lang_materials']:
+                            try:
+                                langs.append(lang['language_and_script']['language'])
+                            except KeyError:
+                                pass
+                            except Exception as e:
+                                print(e)
+
         if len(langs) == 0:
-            resource = self.resource(EXTRACTED_DATA, do_id)
-            if resource != None:
-                if len(resource['lang_materials']) > 0:
-                    for lang in resource['lang_materials']:
-                        try:
-                            langs.append(lang['language_and_script']['language'])
-                        except KeyError:
-                            continue
-        return langs
+            return None
+        else: 
+            return langs
 
 
     def dates(self, EXTRACTED_DATA, do_id):
+        '''Logic based on rules of inheritance: 
+        https://github.com/smith-special-collections/sc-documentation/wiki/Rules-for-description-inheritance-for-digital-object-records'''
         dates = None
-        ao = self.archival_object(EXTRACTED_DATA, do_id)
+        ao = self._archival_object(EXTRACTED_DATA, do_id)
         if ao != None:
             if len(ao['dates']) > 0:
+                # Dates returned as strings
                 dates = ao['dates']
         if dates == None:
-            resource = self.resource(EXTRACTED_DATA, do_id)
+            resource = self._resource(EXTRACTED_DATA, do_id)
             if resource != None:
                 if len(resource['dates']) > 0:
-                    resource['dates']['certainty'] = 'approximate'
+                    resource['dates'][0]['certainty'] = 'approximate'
                     dates = resource['dates']
         return dates
 
@@ -407,20 +500,17 @@ class Transforms():
         repo = None
         do_uri = self.digital_object_uri(EXTRACTED_DATA, do_id)
         repo_num = do_uri.split('/')[2]
-        if repo_num == '2':
-            repo = "Sophia Smith Collection of Women's History"
-        elif repo_num == '3':
-            repo = "Mortimer Rare Book Collection"
-        elif repo_num == '4':
-            repo = "College Archives"
-
-        return repo
+        try: 
+            repo = EXTRACTED_DATA['repositories'][repo_num]
+            return repo
+        except KeyError:
+            return repo
 
 
     def resource_location(self, EXTRACTED_DATA, do_id):
         resource_uri = self.resource_uri(EXTRACTED_DATA, do_id)
         if resource_uri != None:
-            finding_aid_url = 'https://findingaids.smith.edu' + resource_uri
+            finding_aid_url = EXTRACTED_DATA['url_stem'] + resource_uri
             return finding_aid_url
         else:
             return None
@@ -436,7 +526,7 @@ class Transforms():
 
 
     def folder_number(self, EXTRACTED_DATA, do_id):
-        ao = self.archival_object(EXTRACTED_DATA, do_id)
+        ao = self._archival_object(EXTRACTED_DATA, do_id)
         if ao != None:
             try:
                 fol = ao['instances'][0]['sub_container']['type_2'].capitalize()
@@ -450,7 +540,7 @@ class Transforms():
 
     def top_container(self, EXTRACTED_DATA, do_id):
         top_container_string = None
-        ao = self.archival_object(EXTRACTED_DATA, do_id)
+        ao = self._archival_object(EXTRACTED_DATA, do_id)
         if ao != None:
             if len(ao['instances']) > 0:
                 for instance in ao['instances']:
@@ -460,5 +550,14 @@ class Transforms():
                         top_container_string = top_container['display_string']
         return top_container_string
 
+
+    def excerpts(self, EXTRACTED_DATA, do_id):
+        '''Function to determine if (Excerpts) should be appended to title''' 
+        processinfo = self.notes(EXTRACTED_DATA, do_id, 'processinfo')
+        if processinfo != None:
+            for p in processinfo:
+                if 'select material' in p:
+                    excerpts = ' (Excerpts)'
+                    return excerpts
 
 
